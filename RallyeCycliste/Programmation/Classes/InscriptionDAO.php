9d5b186faedde754107cdd0ed1d8e0a15b251ee7
Anthony Geourjon <anthony.geourjon@iut-valence.fr>
2015-02-02 20:09:01 +0100

    Ecriture du code des méthodes, il reste insert et update a terminer.

--- RallyeCycliste/Programmation/Classes/InscriptionDAO.php
+++ RallyeCycliste/Programmation/Classes/InscriptionDAO.php
@@ -5,31 +5,45 @@ class InscriptionDAO extends DAO {
 	
 	// Récupération d'une commande dont on donne le numéro
 	public function getOne($num) {
-		// TODO : À COMPLÉTER
+		$stmt = $this->pdo->prepare ( "SELECT * FROM INSCRIPTION WHERE idInscription=?" );
+		$stmt->execute ( array (
+				$num 
+		) );
+		$row = $stmt->fetch ( PDO::FETCH_ASSOC );
+		return new Inscription ( $row );
 	}
 	
 	// Récupération de toutes les inscriptions de la table
 	public function getAll() {
-		 // TODO : À COMPLÉTER
+		$res = array ();
+		$stmt = $this->pdo->query ( "SELECT * FROM INSCRIPTION ORDER BY idInscription" );
+		foreach ( $stmt->fetchAll ( PDO::FETCH_ASSOC ) as $row )
+			$res [] = new Inscription( $row );
+		return $res;
 	}
 	
 	// Ajout d'une inscription dans la base
 	public function insert($obj) {
-		 // TODO : À COMPLÉTER
+		// TODO : À COMPLÉTER
 	}
 	
 	// Mise à jour de l'objet dans la base
 	public function update($obj) {
-		 // TODO : À COMPLÉTER
+		// TODO : À COMPLÉTER
 	}
 	
 	// Effacement d'une commande : effacer d'abord les lignes de commande, puis la commande elle-mÃªme
 	public function delete($obj) {
-		 // TODO : À COMPLÉTER
+		$stmt = $this->pdo->prepare("DELETE FROM INSCRIPTION WHERE idInscription=?");
+		$res = $stmt->execute(array($obj->idInscription));
+		return $res;
 	}
 	
+	// TODO Regarder comment faire une requête quand on a pas de paramètre
 	public function deleteAll() {
-		// TODO : À COMPLÉTER
+		$stmt = $this->pdo->prepare("DELETE FROM INSCRIPTION");
+		$res = $stmt->execute();
+		return $res;
 	}
 }
 ?>
