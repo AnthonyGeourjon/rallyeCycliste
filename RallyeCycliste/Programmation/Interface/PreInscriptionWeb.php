e1b0c67e51c45fec48c727f0dfe0f86c090f4f7f
Valou <val.andre1@hotmail.fr>
2015-02-25 20:12:25 +0100

    Synchro des interfaces

---
+++ RallyeCycliste/Programmation/Interface/PreInscriptionWeb.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+        <?php
+        	require_once("head.php");
+		?>
+		<title>Pré-Inscription</title>
+
+	</head>
+
+	<body>
+		
+    <div id="corps">
+
+		<?php
+        	require_once("enTete.php");
+		?>
+		<section>
+            <h1>Page de pré-inscritpion</h1>
+                <p>
+                <form action="" method="get">
+            		<input name="Nom" type="text" value="nom" size="20" maxlength="20">
+                    <input name="Prenom" type="text" value="prénom" size="20" maxlength="40">
+                    <input name="Age" type="text" value="age" size="3" maxlength="2">
+                    <input name="Sexe" type="text" value="sexe" size="5" maxlength="5">
+                    <input name="Club" type="text" value="club" size="10" maxlength="40">
+                    <input name="type_parcours" type="text" value="route" size="5" maxlength="5">
+                    <input name="Distance" type="text" value="dst" size="3" maxlength="3">
+                    <input name="Supprimer" type="button" value="Supprimer">
+                    <input name="Copier" type="button" value="Copier">
+                    <input name="AjouterLigne" type="button" value="Ajouter une ligne">  
+                    <input name="Modifier" type="button" value="Modifier">     
+	        	</form>
+        		</p>
+                
+                <p>
+                <form action="" method="get">
+            		<input name="Nom" type="text" value="nom" size="20" maxlength="20">
+                    <input name="Prenom" type="text" value="prénom" size="20" maxlength="40">
+                    <input name="Age" type="text" value="age" size="3" maxlength="2">
+                    <input name="Sexe" type="text" value="sexe" size="5" maxlength="5">
+                    <input name="Club" type="text" value="club" size="10" maxlength="40">
+                    <input name="type_parcours" type="text" value="route" size="5" maxlength="5">
+                    <input name="Distance" type="text" value="dst" size="3" maxlength="3">
+                    <input name="Supprimer" type="button" value="Supprimer">
+                    <input name="Copier" type="button" value="Copier">
+                    <input name="AjouterLigne" type="button" value="Ajouter une ligne"> 
+                    <input name="Modifier" type="button" value="Modifier">       
+	        	</form>
+        		</p>
+                
+                <p>
+                <form action="" method="get">
+            		<input name="Nom" type="text" value="nom" size="20" maxlength="20">
+                    <input name="Prenom" type="text" value="prénom" size="20" maxlength="40">
+                    <input name="Age" type="text" value="age" size="3" maxlength="2">
+                    <input name="Sexe" type="text" value="sexe" size="5" maxlength="5">
+                    <input name="Club" type="text" value="club" size="10" maxlength="40">
+                    <input name="type_parcours" type="text" value="route" size="5" maxlength="5">
+                    <input name="Distance" type="text" value="dst" size="3" maxlength="3">
+                    <input name="Supprimer" type="button" value="Supprimer">
+                    <input name="Copier" type="button" value="Copier">
+                    <input name="AjouterLigne" type="button" value="Ajouter une ligne">
+                    <input name="Modifier" type="button" value="Modifier">       
+	        	</form>
+        		</p>
+                
+                <p>
+                <form>
+                <input name="Valider" type="button" value="Valider">
+                <input name="Supprimer" type="button" value="Supprimer">
+                </form>
+                </p>
+        </section>
+        
+        
+        </div>
+            	
+    </body>
+</html>
+  
---
+++ RallyeCycliste/Programmation/Interface/head.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8" />
+<meta name="author" content="ANDRE Valentin, DUROT Marvin, ANDRE Valentin" />
+
+<link rel="stylesheet" type="text/css" title="style" href="style.css" />
